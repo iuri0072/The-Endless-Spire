b5b289fbacdafc14a92151ffa942d17ebe785c2e
Iuri Prado <iuri0072@hotmail.com>
2023-07-23 17:34:39 -0300

    commit

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -11,8 +11,14 @@ public class LevelGeneration : MonoBehaviour
     public float moveAmountX;
     public float moveAmountY;
 
+    public float minX;
+    public float maxX;
+    public float maxY;
+
     private float timeBtwRoom;
     public float startTimeBtwRoom = 0.25f;
+
+    private bool stopGeneration;
 
     private void Start()
     {
@@ -20,12 +26,14 @@ public class LevelGeneration : MonoBehaviour
         transform.position = startingPositions[randStartingPos].position;
         Instantiate(rooms[0], transform.position, Quaternion.identity);
 
+        stopGeneration = false;
         direction = Random.Range(1, 6);
+        print(direction);
     }
 
     private void Update()
     {
-        if(timeBtwRoom <= 0)
+        if(timeBtwRoom <= 0 && stopGeneration == false)
         {
             Move();
             timeBtwRoom = startTimeBtwRoom;
@@ -40,20 +48,48 @@ public class LevelGeneration : MonoBehaviour
     {
         if(direction == 1 || direction == 2) //move right
         {
-            Vector2 newPos = new Vector2(transform.position.x + moveAmountX, transform.position.y);
-            transform.position = newPos;
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmountX, transform.position.y);
+                transform.position = newPos;
+
+                direction = Random.Range(1, 6);
+                if (direction == 3)
+                    direction = 1;
+                else if (direction == 4)
+                    direction = 5;
+            }
+            else
+                direction = 5;
         }
         else if (direction == 3 || direction == 4) //move left
         {
-            Vector2 newPos = new Vector2(transform.position.x - moveAmountX, transform.position.y);
-            transform.position = newPos;
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmountX, transform.position.y);
+                transform.position = newPos;
+
+                direction = Random.Range(3, 6);
+            }
+            else
+                direction = 5;
         }
         else if (direction == 5) //move up
         {
-            Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmountY);
-            transform.position = newPos;
+            if (transform.position.y < maxY)
+            {
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmountY);
+                transform.position = newPos;
+
+                direction = Random.Range(1, 6);
+            }
+            else //stop generation
+            {
+                stopGeneration = true;
+            }
         }
+
+        print(direction);
         Instantiate(rooms[0], transform.position, Quaternion.identity);
-        direction = Random.Range(1, 6);
     }
 }
