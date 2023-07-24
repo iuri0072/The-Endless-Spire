302a4ca1de34980bca75e941c43375a80a356a30
Iuri Prado <iuri0072@hotmail.com>
2023-07-23 21:21:02 -0300

    commit

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelGeneration : MonoBehaviour
 {
     public Transform[] startingPositions;
-    public GameObject[] rooms;
+    public GameObject[] rooms; // index 0 --> LR, index 1 --> LRB, index 2 --> LRT, index 3 --> LRBT
 
     private int direction;
     public float moveAmountX;
@@ -19,6 +19,7 @@ public class LevelGeneration : MonoBehaviour
     public float startTimeBtwRoom = 0.25f;
 
     private bool stopGeneration;
+    public LayerMask room;
 
     private void Start()
     {
@@ -28,7 +29,6 @@ public class LevelGeneration : MonoBehaviour
 
         stopGeneration = false;
         direction = Random.Range(1, 6);
-        print(direction);
     }
 
     private void Update()
@@ -53,6 +53,9 @@ public class LevelGeneration : MonoBehaviour
                 Vector2 newPos = new Vector2(transform.position.x + moveAmountX, transform.position.y);
                 transform.position = newPos;
 
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
                 direction = Random.Range(1, 6);
                 if (direction == 3)
                     direction = 1;
@@ -69,6 +72,9 @@ public class LevelGeneration : MonoBehaviour
                 Vector2 newPos = new Vector2(transform.position.x - moveAmountX, transform.position.y);
                 transform.position = newPos;
 
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
                 direction = Random.Range(3, 6);
             }
             else
@@ -78,8 +84,25 @@ public class LevelGeneration : MonoBehaviour
         {
             if (transform.position.y < maxY)
             {
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+                //print(roomDetection);
+                if (roomDetection.GetComponent<RoomType>().type != 2 && roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    roomDetection.GetComponent<RoomType>().RoomDestruction();
+
+                    int randTopRoom = Random.Range(2, 4);
+                    //if (randTopRoom == 1)
+                    //    randTopRoom = 2;
+                    Instantiate(rooms[randTopRoom], transform.position, Quaternion.identity);
+                }
+
                 Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmountY);
                 transform.position = newPos;
+
+                int rand = Random.Range(1, 4);
+                if (rand == 2)
+                    rand = 1;
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
 
                 direction = Random.Range(1, 6);
             }
@@ -89,7 +112,5 @@ public class LevelGeneration : MonoBehaviour
             }
         }
 
-        print(direction);
-        Instantiate(rooms[0], transform.position, Quaternion.identity);
     }
 }
