ae7924622429e0276f0497bf7e4c837bb34cb2df
hatbeargames <121828528+hatbeargames@users.noreply.github.com>
2023-08-05 15:47:19 -0400

    Tutorial Scene, Beginning work on music

--- Assets/Scripts/GameMusicPlayer.cs
+++ Assets/Scripts/GameMusicPlayer.cs
@@ -4,22 +4,77 @@ using UnityEngine;
 
 public class GameMusicPlayer : MonoBehaviour
 {
-    private AudioSource _audioSource;
+    [SerializeField]private AudioSource[] _audioSource;
+    public int TrackPlaying;
+    public bool TrackIsPlaying = false;
+    public float transitionTime = 1.25f;
+    public float defaultVolume = 0.4f;
     private void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
-        _audioSource = GetComponent<AudioSource>();
+        PlayMusic(0);
     }
 
-    public void PlayMusic()
+    public void PlayMusic(int index)
     {
-        if (_audioSource.isPlaying) return;
-        _audioSource.Play();
+        if (TrackIsPlaying) return;
+        _audioSource[index].Play();
     }
 
-    public void StopMusic()
+    public void StopMusic(int index)
     {
-        _audioSource.Stop();
+        _audioSource[index].Stop();
     }
+    public void CheckTrackPlaying()
+    {
+        //TrackPlaying = -1;
+        for(int i = 0; i < _audioSource.Length; i++)
+        {
+            if (_audioSource[i].isPlaying)
+            {
+                TrackPlaying = i;
+            }
+        }
+    }
+    public int GetTrackPlaying()
+    {
+        return TrackPlaying;
+    }
+    public void ChangeClip(int from, int to)
+    {
+        //StartCoroutine(Mix)
+        AudioSource nowPlaying = _audioSource[from];
+        AudioSource target = _audioSource[to];
+        if (nowPlaying.isPlaying)
+        {
+            nowPlaying = _audioSource[to];
+            target = _audioSource[from];
+        }
+        StartCoroutine(MixSources(nowPlaying, target));
+    }
+    IEnumerator MixSources(AudioSource nowPlaying, AudioSource target)
+    {
+        float percentage = 0;
+        while (nowPlaying.volume > 0)
+        {
+            nowPlaying.volume = Mathf.Lerp(defaultVolume, 0, percentage);
+            percentage += Time.deltaTime / transitionTime;
+            yield return null;
+        }
+        nowPlaying.Pause();
+        if(target.isPlaying == false)
+        {
+            target.Play();
+        }target.UnPause();
+        percentage = 0;
+        while (target.volume < defaultVolume)
+        {
+            nowPlaying.volume = Mathf.Lerp( 0, defaultVolume, percentage);
+            percentage += Time.deltaTime / transitionTime;
+            yield return null;
+        }
+    }
+
 }
+
 
--- Assets/Scripts/LevelChanger.cs
+++ Assets/Scripts/LevelChanger.cs
@@ -6,10 +6,43 @@ public class LevelChanger : MonoBehaviour
     public Animator animator;
     private int levelToLoad;
     public bool nextLevel;
+    public float distanceFromPlayer;
+    public GameObject player;
+    public float NextLevelRange = 5;
+    public GameObject Exit;
+    public GameMusicPlayer gmp;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Exit = GameObject.FindGameObjectWithTag("Exit");
+        gmp = GameObject.FindGameObjectWithTag("MusicPlayer").GetComponent<GameMusicPlayer>();
+    }
     void Update()
     {
+        //Checks distance from player before proceeding to next level.
+        distanceFromPlayer = Vector3.Distance(player.transform.position, Exit.transform.position);
         //if (Input.GetMouseButtonDown(0))
         //    FadeToNextLevel();
+        if (distanceFromPlayer <= NextLevelRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Debug.Log("exit key pressed");
+                if (Exit.gameObject.tag == "Exit")
+                {
+                    //Debug.Log("Game object tag equals Exit");
+                    if (SceneManager.GetActiveScene().name == "TutorialScene")
+                    {
+                        gmp.CheckTrackPlaying();
+                        int trackIndex = gmp.GetTrackPlaying();
+                        gmp.PlayMusic(trackIndex + 1);
+                        int index = SceneManager.GetActiveScene().buildIndex + 1;
+                        FadeToLevel(index);
+                    }
+                }
+            }
+        }
     }
 
     public void FadeToLevel(int levelIndex)
